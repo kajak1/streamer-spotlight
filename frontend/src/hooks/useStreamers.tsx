982710e9ba11769b2146441c765ba7eefec8c338
kajak1 <kajetan.korzonkiewicz@gmail.com>
2023-07-10 21:33:11 +0200

    refactor: - add useStreamer/s hooks, - extract swr keys, chore: comment out 02_Streamers.ts

--- frontend/src/hooks/useStreamers.tsx
+++ frontend/src/hooks/useStreamers.tsx
@@ -1,10 +1,11 @@
 import useSWR from "swr";
 import { streamersService } from "../services/streamers.service";
 import { Streamer } from "../shared.types";
+import { SWR_KEYS } from "../swr-keys";
 
 function useStreamers() {
 	const { data, isLoading, error, isValidating, mutate } = useSWR<Streamer[]>(
-		"api/getAll",
+		SWR_KEYS.GET_ALL,
 		streamersService.getAll
 	);
 
