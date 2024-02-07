d3ba2f4e83a9b499fb30f96eb08e4d27e855f706
kajak1 <kajetan.korzonkiewicz@gmail.com>
2024-02-07 18:32:12 +0100

    Add eslint plugins

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -3,7 +3,19 @@ module.exports = {
 		node: true,
 		es2021: true,
 	},
-	extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+	extends: [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/recommended",
+		"plugin:import/recommended",
+		"plugin:import/typescript",
+	],
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		ecmaVersion: "latest",
+		sourceType: "module",
+	},
+	plugins: ["@typescript-eslint"],
+	root: true,
 	overrides: [
 		{
 			env: {
@@ -15,15 +27,17 @@ module.exports = {
 			},
 		},
 	],
-	parser: "@typescript-eslint/parser",
-	parserOptions: {
-		ecmaVersion: "latest",
-		sourceType: "module",
-	},
-	plugins: ["@typescript-eslint"],
 	rules: {
 		"no-debugger": "warn",
 		"no-empty-function": "off",
 		"@typescript-eslint/no-empty-function": "warn",
+		"@typescript-eslint/ban-ts-comment": "warn",
+		"import/no-named-as-default-member": "off",
+	},
+	settings: {
+		"import/resolver": {
+			typescript: true,
+			node: true,
+		},
 	},
 };
